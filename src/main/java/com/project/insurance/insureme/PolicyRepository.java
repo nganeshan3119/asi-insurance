d55be3e23162c560a5743114c96ba12aa6f73793
nganeshan3119 <137711373+nganeshan3119@users.noreply.github.com>
2024-03-13 23:07:12 +0530

    Delete src directory

--- src/main/java/com/project/insurance/insureme/PolicyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.insurance.insureme;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PolicyRepository extends JpaRepository<Policy,Integer>{
-
-}
